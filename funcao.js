efb59b9d66f4dcfe7ef087856ef3d54d546b1d7c
chayennefelix <113393167+chayennefelix@users.noreply.github.com>
2022-09-19 20:00:37 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,19 @@
+ano=prompt("Informe o ano de nascimento")
+nome=prompt("Informe seu Nome")
+atual= 2022
+idade= atual-ano
+console.log("Sr.Sra", nome, "Sua idade é", idade, "anos")
+if (idade>=18){
+    alert("Você é maior de idade")
+}
+else{
+    alert("Você é menor de idade")
+}
+
+
+v1=prompt("Informe um valor")
+v2=prompt("Informe outro valor")
+atual= 2022
+soma= parseInt(v1)+parseInt(v2)
+alert(" A soma é" + soma)
+
